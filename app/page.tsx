2b79f1eed01403be08ae371478208a468161dc1b
Deven Mallamo <deven@Devens-Mac-mini.local>
2024-10-12 09:06:57 -0400

    temp layout push to save

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-// 'use client'
-
-import styles from "./landingpage.module.css";
-// import BrainLightLogo from "@/app/components/BrainLight";
+import "./landingpage.css";
 import Link from "next/link";
 import BrainLight from "./assets/images/BrainLight.png";
 import Logo from "./assets/images/Logo.png"
@@ -10,20 +7,20 @@ import Image from 'next/image';
 export default function Home() {
     return (
       <div>
-        <nav className={'p-3'}>
+        <div className="p-3">
           <Image src={Logo} alt="StockWize Logo" width={50}  />
-          <span className={`p-2 ${styles.companyName}` }>StockWize</span>
-        </nav>
-        <div className={`position-absolute top-50 start-50 translate-middle container-sm`}>
-          <div className={'d-flex flex-row justify-content-around'}>
-            <div id= "textContainer" className={`d-flex flex-column`}>
-              <h1 className={`pb-3 ${styles.title}`}>Empower <br/> Your <br/> Investments</h1>
-              <p className={`pb-3 ${styles.missionStatement} `}>
+          <span className="p-2 companyName">StockWize</span>
+        </div>
+        <div className="position-absolute top-50 start-50 translate-middle container-sm">
+          <div className='d-flex flex-row justify-content-around'>
+            <div id= "textContainer" className="d-flex flex-column">
+              <h1 className="pb-3 title">Empower <br/> Your <br/> Investments</h1>
+              <p className="pb-3 missionStatement">
                 Stockwise unlocks your financial potential, 
                 empowering you to make smarter investment decisions with ease.
               </p>
-              <Link href={'/overview'}>
-                <button className={`p-10 w-100 ${styles.button}`} >
+              <Link href={'content/overview'}>
+                <button className="p-10 w-100 button">
                   Search for Stock Tickers & Companies
                 </button>
               </Link>
