71adf239406811ac8cfa24befab078ec30ba48c3
bdegrieck <bendegrieck@comcast.net>
2024-10-16 17:24:08 -0400

    eda helpers

---
+++ BackEnd/Eda/helpers.py
@@ -0,0 +1,57 @@
+import numpy as np
+from pydantic import BaseModel
+from pydantic.v1 import validator
+
+from BackEnd.error import ArrayShapeError
+
+class ConfidenceIntervalBounds(BaseModel):
+    """
+    lower_bounds: lower bounds of the coefficients
+    upper_bounds: upper bounds of the coefficients
+    """
+    lower_bound: np.array
+    upper_bound: np.array
+
+    @validator("lower_bound", "upper_bound")
+    def check_bounds_ndim(cls, bounds):
+        if bounds.ndim != 1:
+            raise ArrayShapeError(array_size=bounds.ndim, allowed_size=1)
+        return bounds
+
+    class Config:
+        arbitrary_types_allowed = True
+
+class CorrelationData(BaseModel):
+    """
+    coefficients: Coefficients of the auto correlation models
+    confidence_intervals: confidence intervals of the coefficients
+    """
+    coefficients: np.array
+    confidence_interval: ConfidenceIntervalBounds
+
+    @validator("coefficients")
+    def check_confidence_interval_shape(cls, coef):
+        if coef.ndim != 1:
+            raise ArrayShapeError(array_size=coef.ndim, allowed_size=1)
+        return coef
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+def get_significant_lags(coef: CorrelationData) -> list[int]:
+    """
+
+    Args:
+        coef:
+
+    Returns:
+
+    """
+    sig_lags = []
+    for i in range(1, len(coef.coefficients)):
+        if coef.coefficients[i] > (coef.confidence_interval.upper_bound[i] - coef.coefficients[i]):
+            sig_lags.append(i)
+        elif coef.coefficients[i] < (coef.confidence_interval.lower_bound[i] - coef.coefficients[i]):
+            sig_lags.append(i)
+    return sig_lags
