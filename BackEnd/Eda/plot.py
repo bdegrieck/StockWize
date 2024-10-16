626ace9688ee20831512f48480ad13477279e474
bdegrieck <bendegrieck@comcast.net>
2024-10-16 17:23:27 -0400

    eda file change and new plot

--- BackEnd/Eda/plot.py
+++ BackEnd/Eda/plot.py
@@ -2,8 +2,9 @@ import pandas as pd
 from plotly.subplots import make_subplots
 from pydantic.v1 import BaseModel
 import plotly.graph_objects as go
+import statsmodels.api as sma
 
-class Graph(BaseModel):
+class MSTLGraph(BaseModel):
     """
     xaxis_data (pd.Series) data of the x-axis
     yaxis_data (pd.Series) data of the y-axis
@@ -21,11 +22,37 @@ class Graph(BaseModel):
         arbitrary_types_allowed = True
 
 
-def plot(graphs: list[Graph], main_title: str):
+class ACFGraph(BaseModel):
+    """
+    data (pd.Series): Series of data you want to plot
+    title (str): title of the plot
+    """
+    data: pd.Series
+    title: str
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+
+class PACFGraph(BaseModel):
+    """
+    data (pd.Series): Series of data you want to plot
+    title (str): title of the plot
+    """
+    data: pd.Series
+    title: str
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+
+def plot(graphs: list[MSTLGraph], main_title: str):
     """
     Function to plot n_rows sub graphs
     Args:
-        graphs (Graph): Graph object that contains the data and titles
+        graphs (MSTLGraph): Graph object that contains the data and titles
         main_title (str): title of the chart
 
     Returns:
@@ -46,4 +73,30 @@ def plot(graphs: list[Graph], main_title: str):
         showlegend=False
     )
 
+    return fig
+
+
+def plot_acf(graph: ACFGraph):
+    """
+    Plotting acf graphs
+    Args:
+        graph (ACFGraph): Acf graph object you want to plot
+
+    Returns:
+        fig - figure of the plot
+    """
+    fig = sma.graphics.tsa.plot_acf(x=graph.data, title=graph.title)
+    return fig
+
+
+def plot_pacf(graph: PACFGraph):
+    """
+    Plotting acf graphs
+    Args:
+        graph (ACFGraph): Pacf graph object you want to plot
+
+    Returns:
+        fig - figure of the plot
+    """
+    fig = sma.graphics.tsa.plot_pacf(x=graph.data, title=graph.title)
     return fig
