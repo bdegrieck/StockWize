afe79c5b7b907d0c5205ab6d715e4dbce0fcabc4
Deven Mallamo <neved1234@gimail.com>
2024-10-21 11:49:27 -0400

    Begin forecasted page

--- app/content/template.tsx
+++ app/content/template.tsx
@@ -13,7 +13,7 @@ import Image, { StaticImageData } from "next/image";
 import "./layout.css";
 import { useRouter, useSearchParams, usePathname } from "next/navigation";
 import { FormEvent, useState } from "react";
-import ForecastedGraph from "@/app/components/ForecastedGraph";
+
 
 export default function ContentLayout({
   children,
@@ -31,8 +31,7 @@ export default function ContentLayout({
   const content =
     company === "" ? (
       <div className="h-100 w-100 d-flex justify-content-center align-items-center">
-        {/* <p>Search for a Company or Stock Symbol to Get Started</p> */}
-        <LineGraph />
+        <p>Search for a Company or Stock Symbol to Get Started</p>
       </div>
     ) : (
       <div className="h-100 d-flex flex-column">
@@ -157,42 +156,4 @@ function NavBarItem({
     </Link>
   );
 }
-function LineGraph() {
-  return (
-    <div className="graphKPI d-flex flex-column justify-content-between bg-white rounded-3 shadow-lg m-0 p-0">
-      <div className="stock-text d-flex align-items-center justify-content-between w-100 p-3">
-        <div className="left d-flex align-items-end justify-content-between">
-          <h1 className="h1 fw-bold pe-3">Ticker</h1>
-          <h2 className="h2">Company</h2>
-        </div>
-        <div className="right d-flex justify-content-between align-self-end">
-          <h3 className="h3 fw-bold pe-3">000.00</h3>
-          <p className="p text-secondary">+0.00</p>
-        </div>
-      </div>
-      <div className="descriptor d-flex align-items-center justify-content-between">
-        <h3 className="h3 text-secondary ps-3">Chart Type</h3>
-      </div>
-      <ForecastedGraph />
-      {/* <LineChartComponent /> */}
-      <div className="metrics d-flex w-100 align-items-center justify-content-evenly p-3">
-        <div className="value d-flex align-items-center justify-content-between">
-          <h3 className="h3 text-secondary pe-2">Open</h3>
-          <h2 className="h2 fw-bold text-dark">000.00</h2>
-        </div>
-        <div className="value d-flex align-items-center justify-content-between">
-          <h3 className="h3 text-secondary pe-2">High</h3>
-          <h2 className="h2 fw-bold text-dark">000.00</h2>
-        </div>
-        <div className="value d-flex align-items-center justify-content-between">
-          <h3 className="h3 text-secondary pe-2">Low</h3>
-          <h2 className="h2 fw-bold text-dark">000.00</h2>
-        </div>
-        <div className="value d-flex align-items-center justify-content-between">
-          <h3 className="h3 text-secondary pe-2">Close</h3>
-          <h2 className="h2 fw-bold text-dark">000.00</h2>
-        </div>
-      </div>
-    </div>
-  );
-}
+
